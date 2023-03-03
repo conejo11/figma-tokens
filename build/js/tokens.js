ffe62f0b2a649ee0894d8329da2829cde4448465
Gabriel Conejo <gabrielgconejo@gmail.com>
2023-03-03 10:15:39 -0300

    Testing npm package

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 03 Mar 2023 12:52:24 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#121214",
+  "ColorGray900": "#1c1c1f",
+  "ColorPurple500": "#8257e6"
+};
